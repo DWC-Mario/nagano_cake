6b3579eaab8d9d507f32790a6b6abd8644aec741
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-05-15 07:05:40 +0000

    Add CartItems Orders model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,16 @@
 class Order < ApplicationRecord
-
-  enum how_to_pay:   { "銀行振込": 0, "クレジットカード": 1 }
-  enum order_status: { "入金待ち": 0, "入金確認": 1, "製造中": 2, "発送準備中": 3, "発送済み": 4 }
-
+  
+  enum how_to_pay: { 
+    transfer: 0, 
+    credit_card: 1 
+  }
+  
+  enum order_status: {
+    payment_waiting: 0,
+    paid_up: 1,
+    in_production: 2,
+    preparing: 3,
+    shipped: 4
+  }
+  
 end
