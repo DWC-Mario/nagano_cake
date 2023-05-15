1f9be45ed4fd4f4a21dcd071d75f6796f34eafdf
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-05-15 08:53:18 +0000

    [make_controller2]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,19 @@
 class Admin::ItemsController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
 end
