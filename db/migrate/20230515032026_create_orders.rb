7027c4c8b3c77bb5cf0b04be7045b57057c06f11
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-05-15 03:41:40 +0000

    Add Order model

--- db/migrate/20230515032026_create_orders.rb
+++ db/migrate/20230515032026_create_orders.rb
@@ -1,7 +1,16 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-
+      
+      t.integer :customer_id,  null: false
+      t.integer :post_code,    null: false
+      t.string  :address,      null: false
+      t.string  :address_name, null: false
+      t.integer :postage,      null: false
+      t.integer :total_price,  null: false
+      t.integer :how_to_pay,   null: false, default: 0
+      t.integer :order_status, null: false, default: 0
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_005443) do
+ActiveRecord::Schema.define(version: 2023_05_15_032026) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,19 @@ ActiveRecord::Schema.define(version: 2023_05_15_005443) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "post_code", null: false
+    t.string "address", null: false
+    t.string "address_name", null: false
+    t.integer "postage", null: false
+    t.integer "total_price", null: false
+    t.integer "how_to_pay", default: 0, null: false
+    t.integer "order_status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
