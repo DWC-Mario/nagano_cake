c020acf3be5b6ba50dc9c2627e8fae5654f984a1
EC2 Default User <ec2-user@ip-172-31-11-222.ap-northeast-1.compute.internal>
2023-05-15 08:27:43 +0000

    “controllersakusei”

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  get 'addresses/index'
-  get 'addresses/edit'
-  get 'addresses/create'
-  get 'addresses/update'
-  get 'addresses/destroy'
-  get 'homes/top'
-  get 'homes/about'
+  
 devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
