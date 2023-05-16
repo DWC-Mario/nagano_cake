72e6006bd7d8fa70a684c25c2568ddc1e514942d
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-05-16 09:06:57 +0000

    [viewfile]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items=Item.all
   end
 
   def show
+     @item = Item.find(params[:id])  
   end
+  
+   private
+  def item_params
+    params.require(:item).permit(:title, :body, :image)  end
 end
