c957312498baa110fba3eafd3f05387a9edf9a88
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-05-16 10:54:03 +0000

    seed追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_16_014442) do
+ActiveRecord::Schema.define(version: 2023_05_16_104806) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 2023_05_16_014442) do
     t.integer "order_id", null: false
     t.integer "item_id", null: false
     t.integer "count", null: false
-    t.integer "tax_inculuded_price", null: false
+    t.integer "tax_included_price", null: false
     t.integer "production_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(email:"fa@fa.com",password:"ffffff")
+Customer.create(last_name:"山田",first_name:"太郎",last_name_kana:"ヤマダ",first_name_kana:"タロウ",post_code:"1234567",
+                address:"長野県松本市",phone_number:"12345678900",is_active: true,email:"f@f.com",password:"123456")
+
+Item.create(item_name: "シュークリーム" , item_explanation: "説明", genre_id: 1, price: 500, is_active: true)
+
+Order.create(customer_id: 1,post_code:"1234567",address_name:"山田",postage: 800,total_price: 1300,how_to_pay: 0,order_status: 0)
+
+OrderingItem.create(order_id: 1, item_id: 1, count: 1, tax_included_price: 550, production_status: 0)
+
+Genre.create(genre_name:"焼き菓子")
+
+Address.create(customer_id: 1, post_code: "1234567", address: "長野県松本市", address_name: "山田")
+
+CartItem.create(item_id: 1,customer_id: 1,count: 1)
