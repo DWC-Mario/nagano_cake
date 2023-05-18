550eebc5e43d6f6fdea6b059f077291f2e70b417
touchselect <kir191474@gmail.com>
2023-05-18 10:56:17 +0000

    second commit

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,10 +18,35 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    customer_path
+  end
+  
+def customer_state
+  # 入力されたemailからアカウントを1件取得
+  @customer = Customer.find_by(email: params[:customer][:email])
+  # アカウントを取得できなかった場合、このメソッドを終了する
+  return unless @customer
+  # 取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+  if @customer.valid_password?(params[:customer][:password])
+    # パスワードが一致した場合の処理内容を記述する
+    if @customer.is_active
+      # アカウントが有効な場合の処理内容を記述する
+      redirect_to customer_session
+    else
+      # アカウントが無効（退会済み）の場合の処理内容を記述する
+      redirect_to  new_customer_registration_path
+    end
+  else
+    # パスワードが一致しなかった場合の処理内容を記述する
+    
+  end
+end
+  
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,4 @@
 class Address < ApplicationRecord
   
   belongs_to :customer
-  has_many :orders
-  
 end
