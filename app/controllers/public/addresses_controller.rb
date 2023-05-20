8b6ee2952bdbf21e2e7ea12e8ce9c1c9e3bfd1c6
EC2 Default User <ec2-user@ip-172-31-11-222.ap-northeast-1.compute.internal>
2023-05-20 01:20:52 +0000

    2

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,45 @@
 class Public::AddressesController < ApplicationController
   def index
+   @addresses = Address.all
+   @address = 
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:notice] = "新しい配送先を登録しました."
+      redirect_to '/addresses'
+    else
+      @address = Address.all
+      render :index
+    end
   end
 
   def update
+     @address = Address.find(params[:id])
+      if @address.update(address_params)
+        flash[:notice] = "登録情報を更新しました."
+        redirect_to '/addresses'
+      else
+        render :edit
+        @address = Address.all
+      end
   end
+  
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+     redirect_to '/addresses'
   end
+
+  private
+    def address_params
+      params.require(:address).permit(:post_code, :address, :address_name)
+    end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   
   belongs_to :customer
   
+  validates :post_code, numericality: { only_integer: true}
+  validates :address, presence: true
+  validates :address_name, presence: true
+  
 end
