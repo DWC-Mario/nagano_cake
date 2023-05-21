1e2f75926deac9cd8cd189cf2887d83f8dc70ff9
touchselect <kir191474@gmail.com>
2023-05-21 15:07:05 +0000

    save

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,14 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order = Order.find(params[:id])
+    @ordering_items = @order.ordering_items
+    @ordering_items.update(order_detail_params)
+    redirect_to admin_order_path
   end
+end
+
+private
+
+def order_detail_params
+  params.require(:ordering_item).permit(:production_status)
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,26 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @formatted_post_code = @order.post_code.insert(3, "-")
+    @ordering_items = @order.ordering_items
+    @sum = @ordering_items.inject(0) { |sum, ordering_item| sum + ordering_item.subtotal }
   end
 
   def update
+    @order = Order.find(params[:id])
+    @ordering_items = @order.ordering_items
+    if @order.update(order_params)
+      redirect_to admin_order_path
+    else
+      render :show
+    end
   end
+
+
+private
+
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+  
 end
